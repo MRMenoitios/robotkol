7dcdf37656f4cc78fdaaee4312345c458e67a894
MRMenoitios <muhammet2006adibas@gmail.com>
2026-05-12 19:22:35 +0300

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -12,12 +12,13 @@ const int pinKiskac = 9;
 const int pinBuzzer = 10;
 
 // Mevcut ve Hedef Pozisyonlar
-int suankiTaban = 90, hedefTaban = 90;
-int suankiOmuz = 90, hedefOmuz = 90;
-int suankiDirsek = 90, hedefDirsek = 90;
+int suankiTaban = 90;
+int suankiOmuz = 90;
+int hedefTaban = 90;
+int hedefOmuz = 90;
 
 unsigned long sonHareketZamani = 0;
-const int hareketGecikmesi = 15; 
+const int hareketGecikmesi = 15; // Her 15ms'de 1 derece hareket (Hız ayarı)
 
 void setup() {
   Serial.begin(9600);
@@ -28,45 +29,46 @@ void setup() {
   motorDirsek.attach(pinDirsek);
   motorKiskac.attach(pinKiskac);
 
+  // Başlangıç Pozisyonu
   motorTaban.write(suankiTaban);  
   motorOmuz.write(suankiOmuz);   
-  motorDirsek.write(suankiDirsek); 
+  motorDirsek.write(90); 
   motorKiskac.write(30); 
 
-  tone(pinBuzzer, 1500, 100); delay(150);
+  // Açılış Sesi
+  tone(pinBuzzer, 1500, 100);
+  delay(150);
   tone(pinBuzzer, 2000, 200);
 }
 
 void loop() {
+  // Yeni hedef kontrolü
   if (Serial.available() > 0) {
     int x = Serial.parseInt(); 
     int y = Serial.parseInt();
-    int z = Serial.parseInt(); // Z-Ekseni (Uzanma)
 
     if (x > 0 && y > 0) {
-      tone(pinBuzzer, 1200, 50);
+      tone(pinBuzzer, 1200, 50); // Komut alındı sesi
       hedefTaban = map(x, 0, 640, 180, 0); 
       hedefOmuz = map(y, 0, 480, 130, 50); 
-      hedefDirsek = map(z, 0, 200, 40, 140); // Z derinliği Dirsek motoruna
     }
   }
 
+  // Yumuşak Hareket (Smoothing) Mantığı
   if (millis() - sonHareketZamani > hareketGecikmesi) {
     bool hareketVar = false;
 
+    // Taban motoru ilerlemesi
     if (suankiTaban < hedefTaban) { suankiTaban++; hareketVar = true; }
     else if (suankiTaban > hedefTaban) { suankiTaban--; hareketVar = true; }
 
+    // Omuz motoru ilerlemesi
     if (suankiOmuz < hedefOmuz) { suankiOmuz++; hareketVar = true; }
     else if (suankiOmuz > hedefOmuz) { suankiOmuz--; hareketVar = true; }
-
-    if (suankiDirsek < hedefDirsek) { suankiDirsek++; hareketVar = true; }
-    else if (suankiDirsek > hedefDirsek) { suankiDirsek--; hareketVar = true; }
 
     if (hareketVar) {
       motorTaban.write(suankiTaban);
       motorOmuz.write(suankiOmuz);
-      motorDirsek.write(suankiDirsek);
       sonHareketZamani = millis();
     }
   }
